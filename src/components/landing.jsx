f479673cb3f0f892bd5c9701c547b2adff18dd33
Krish Mehta <krish1604mehta@gmail.com>
2025-02-21 02:57:30 +0530

    arrow change

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -19,14 +19,14 @@ const Landing = () => {
           
           <div className="flex justify-center flex-1">
             <div className="flex gap-4 justify-center">
-              <a href="https://www.linkedin.com/in/krish-mehta-7a4037252/" className="hover:text-orange-500 transition-colors flex items-center gap-1">LINKEDIN <ArrowUpRight className="w-4 h-4 rotate-12" /></a>
-              <a href="https://www.instagram.com/_krish1604/" className="hover:text-orange-500 transition-colors flex items-center gap-1">INSTAGRAM <ArrowUpRight className="w-4 h-4 rotate-12" /></a>
-              <a href="https://github.com/krish-1604" className="hover:text-orange-500 transition-colors flex items-center gap-1">GITHUB <ArrowUpRight className="w-4 h-4 rotate-12" /></a>
+              <a href="https://www.linkedin.com/in/krish-mehta-7a4037252/" className="hover:text-orange-500 transition-colors flex items-center gap-1">LINKEDIN <ArrowUpRight className="w-4 h-4 rotate-0" /></a>
+              <a href="https://www.instagram.com/_krish1604/" className="hover:text-orange-500 transition-colors flex items-center gap-1">INSTAGRAM <ArrowUpRight className="w-4 h-4 rotate-0" /></a>
+              <a href="https://github.com/krish-1604" className="hover:text-orange-500 transition-colors flex items-center gap-1">GITHUB <ArrowUpRight className="w-4 h-4 rotate-0" /></a>
             </div>
           </div>
           
           <button className="border-2 border-orange-500 bg-[#393939] text-white px-4 py-2 rounded-full hover:bg-orange-500 transition-all flex items-center gap-1" onClick={() => { window.location.href = "mailto:krish1604mehta@gmail.com?subject=Interested in Your Work&body=Hi Krish,"; }}>
-            LET'S TALK <ArrowUpRight className="w-4 h-4 rotate-12" />
+            LET'S TALK <ArrowUpRight className="w-4 h-4 rotate-0" />
           </button>
         </nav>
         <main className="flex flex-col lg:flex-row items-center justify-between gap-6 pt-8">
